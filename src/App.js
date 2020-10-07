afee96f42373fe467ae867cc8f2ed5d35d932237
panvicka <panvicka@gmail.com>
2020-10-07 22:13:00 +0200

    add styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React from "react";
+import "./assets/css/style.css"
 
+/* class App extends React.Component {
+  constructor(props) {
+      super(props); // call constructor of super class 
+      this.state = {
+          title: "hello 2 "
+      }
+  }
 
-function App() {
-  return <div>Hello</div>;
+  render() {
+    return <div>{this.state.title}</div>;
+  }
+} */
+
+function App({ title }) {
+  return (
+    <div>
+      <div className="box">
+        Hello {title}
+      </div>
+    </div>
+  );
 }
-
-
-
 
 export default App;
